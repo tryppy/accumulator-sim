8bbf98a308af849e068cf934e72a4bf847eef1c9
tryppy <pavan.wasan@gmail.com>
2025-02-01 01:16:33 +0800

    Update layout for dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body className="bg-gray-900 text-gray-100">{children}</body>
     </html>
   );
 }
